0b10ec51def47b9641cdfbf19356858ab21d9f16
vvercasson <victor.vercasson@etu.u-bordeaux.fr>
2022-01-03 01:58:29 +0100

    status bar fix

--- src/main/java/fr/ubx/poo/ubomb/engine/StatusBar.java
+++ src/main/java/fr/ubx/poo/ubomb/engine/StatusBar.java
@@ -76,7 +76,7 @@ public class StatusBar {
     }
 
     public void update(Game game) {
-        updateLevel(1);
+        updateLevel(game.currentLevel);
         lives.setText(String.valueOf(game.getPlayer().getLives()));
         bombRange.setText(String.valueOf(game.bombRange));
         availableBombs.setText(String.valueOf(game.bombCapacity));
